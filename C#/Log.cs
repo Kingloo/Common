969f59d0afd125aa778e6d34b32f92a1c0451603
Kingloo <k1ngloo@hotmail.com>
2019-11-21 23:45:55 +0100

    Stop catching FileNotFoundException in Log.cs

--- C#/Log.cs
+++ C#/Log.cs
@@ -144,7 +144,6 @@ namespace .Common
                     sw.Flush();
                 }
             }
-            catch (FileNotFoundException) { }
             catch (IOException) { }
             finally
             {
@@ -175,7 +174,6 @@ namespace .Common
                     await sw.FlushAsync().ConfigureAwait(false);
                 }
             }
-            catch (FileNotFoundException) { }
             catch (IOException) { }
             finally
             {
