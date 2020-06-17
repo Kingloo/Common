127d8bb2058c56d49fe2a2d355e67a4be6b812cc
Kingloo <k1ngloo@hotmail.com>
2020-06-17 16:25:00 +0200

    Space now shows totals as well

--- PowerShell/Space.ps1
+++ PowerShell/Space.ps1
@@ -1,12 +1,14 @@
+#$format = "#,#.#"
 $format = "#.#"
 $gib = 1024 * 1024 * 1024
 
 $totalUsed = 0
 $totalFree = 0
+$totalSize = 0
 
 Write-Host ""
 
-Write-Host "Space on Mounted Shares (used, free) (GiBs)"
+Write-Host "Space on Mounted Shares (used, free, total) (GiBs)"
 
 Write-Host ""
 
@@ -16,20 +18,27 @@ foreach ($drive in $drives)
 {
 	$free = $drive.FreeSpace
 	$used = $drive.Size - $drive.FreeSpace
+	$size = $drive.Size
 
 	$totalUsed += $used
 	$totalFree += $free
+	$totalSize += $size
 
 	$hrUsed = ($used / $gib).ToString($format)
 	$hrFree = ($free / $gib).ToString($format)
-		
-	Write-Host "$($drive.DeviceID)`t`t$($hrUsed)`t`t$($hrFree)`t`t$($drive.VolumeName)`t`t$($drive.ProviderName)"
+	$hrSize = ($size / $gib).ToString($format)
+	
+	Write-Host "$($drive.DeviceID)`t`t$($hrUsed)`t`t$($hrFree)`t`t$($hrSize)`t`t$($drive.VolumeName)`t`t$($drive.ProviderName)"
 }
 
 Write-Host ""
 
 $hrTotalUsed = ($totalUsed / $gib).ToString($format)
 $hrTotalFree = ($totalFree / $gib).ToString($format)
+$hrTotalSize = ($totalSize / $gib).ToString($format)
 
-Write-Host "Total`t`t$($hrTotalUsed)`t`t$($hrTotalFree)"
+# https://stackoverflow.com/questions/21838419/create-table-with-variables-in-powershell
+
+Write-Host "Total`t`t$($hrTotalUsed)`t`t$($hrTotalFree)`t`t$($hrTotalSize)"
+
 Write-Host ""
