1a1e3d4d4db8362f6fc82ab07dc5f43ccc9a639c
Kingloo <k1ngloo@hotmail.com>
2020-07-18 23:26:26 +0200

    Add first JavaScript file
    Get a properly formatted datetime from a Date()

---
+++ JavaScript/getDateHumanReadable.js
@@ -0,0 +1,15 @@
+function getDateTime(d) { // new Date()
+    let year = d.getFullYear();
+    let month = d.getMonth();
+    let date = d.getDate();
+    let hours = d.getHours();
+    let minutes = d.getMinutes();
+    let seconds = d.getSeconds();
+    let monthHr = month < 10 ? "0" + month : month;
+    let dateHr = date < 10 ? "0" + date : date;
+    let hoursHr = hours < 10 ? "0" + hours : hours;
+    let minutesHr = minutes < 10 ? "0" + minutes : minutes;
+    let secondsHr = seconds < 10 ? "0" + seconds : seconds;
+
+    return year + "-" + monthHr + "-" + dateHr + " " + hoursHr + ":" + minutesHr + ":" + secondsHr;
+}
