bf58df35707e8c83ff9bef2ac195afdf8cc87501
Kingloo <k1ngloo@hotmail.com>
2020-11-20 07:45:31 +0100

    Add UserAgents.cs

---
+++ C#/UserAgents.cs
@@ -0,0 +1,8 @@
+namespace .Common
+{
+    public static class UserAgents
+    {
+        public const string Firefox83 = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0";
+        public const string MsEdge87 = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36 Edg/87.0.664.41";
+    }
+}
