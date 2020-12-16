75d6d6d5a2f31781c715510a46add186d003d86b
Kingloo <k1ngloo@hotmail.com>
2020-12-17 00:39:25 +0100

    Some new user agents, more consistent naming

--- C#/UserAgents.cs
+++ C#/UserAgents.cs
@@ -4,11 +4,12 @@ namespace .Common
 {
     public static class UserAgents
     {
-        public const string Firefox83 = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0";
-        public const string MsEdge87 = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36 Edg/87.0.664.41";
-        public const string MacOSXSafari13_1 = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15";
-        public const string GoogleChrome85 = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36";
-        public const string Opera66 = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36 OPR/66.0.3515.72";
+        public const string Firefox_83_Windows = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0";
+        public const string Edge_87_Windows = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36 Edg/87.0.664.41";
+        public const string Safari_13_1_MacOSX = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15";
+        public const string Chrome_85_Windows = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36";
+        public const string Opera_66_Windows = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36 OPR/66.0.3515.72";
+        public const string Firefox_78ESR_Linux = "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0";
 
         public static string GetRandomUserAgent()
         {
@@ -16,11 +17,12 @@ namespace .Common
             
             return System.Environment.TickCount.ToString().Last().ToString() switch
             {
-                "1" => Firefox83,
-                "2" => MsEdge87,
-                "3" => MacOSXSafari13_1,
-                "4" => GoogleChrome85,
-                _ => Opera66
+                "1" => Firefox_83_Windows,
+                "2" => Edge_87_Windows,
+                "3" => Safari_13_1_MacOSX,
+                "4" => Chrome_85_Windows,
+                "5" => Opera_66_Windows,
+                _ => Firefox_78ESR_Linux
             };
         }
     }
