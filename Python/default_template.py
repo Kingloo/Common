f7201987a568df76aeab71f050bb7cfdb850f311
Kingloo <k1ngloo@hotmail.com>
2021-05-16 04:46:21 +0200

    Replace spaces with tabs

--- Python/default_template.py
+++ Python/default_template.py
@@ -1,13 +1,13 @@
 import sys
 
 def process():
-    print("Hello, World!")
+	print("Hello, World!")
 
 def main(args):
-    try:
-        process() # deal with arguments here
-    except Exception as e:
-        print(str(e))
+	try:
+		process() # deal with arguments here
+	except Exception as e:
+		print(str(e))
 
 if __name__ == "__main__":
-    main(sys.argv[1:]) # 1: removes the filename from argv
+	main(sys.argv[1:]) # 1: removes the filename from argv
