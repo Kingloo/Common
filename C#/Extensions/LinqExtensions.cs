250da754f3245dcf4d44282395451029b93d1981
Kingloo <k1ngloo@hotmail.com>
2021-07-17 02:07:09 +0200

    Remove unnecessary seeding for Randoms

--- C#/Extensions/LinqExtensions.cs
+++ C#/Extensions/LinqExtensions.cs
@@ -4,31 +4,20 @@ using System.Linq;
 
 namespace .Extensions
 {
-    public static class LinqExtensions
-    {
-        public static IList<T> Randomise<T>(this IList<T> list)
-        {
-            Random random = new Random(GetSeed());
+	public static class LinqExtensions
+	{
+		public static IList<T> Randomise<T>(this IList<T> list)
+		{
+			Random random = new Random();
 
-            return list.OrderBy(x => random.Next()).ToList();
-        }
+			return list.OrderBy(x => random.Next()).ToList();
+		}
 
-        public static IEnumerable<T> Randomise<T>(this IEnumerable<T> enumerable)
-        {
-            Random random = new Random(GetSeed());
+		public static IEnumerable<T> Randomise<T>(this IEnumerable<T> enumerable)
+		{
+			Random random = new Random();
 
-            return enumerable.ToList().OrderBy(x => random.Next());
-        }
-
-        private static int GetSeed()
-        {
-            string ticks = DateTimeOffset.Now.Ticks.ToString();
-
-            int numberOfDigits = Int32.Parse(ticks[^1..]) + 1;
-
-            Int64 seed64 = Int64.Parse(ticks[^numberOfDigits..]);
-
-            return (seed64 < Int32.MaxValue) ? Convert.ToInt32(seed64) : Int32.MaxValue;
-        }
-    }
+			return enumerable.ToList().OrderBy(x => random.Next());
+		}
+	}
 }
