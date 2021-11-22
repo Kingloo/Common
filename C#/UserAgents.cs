09bdb466a2ef00652f00a7fb5284a855919ffdd2
Kingloo <k1ngloo@hotmail.com>
2021-11-22 23:54:44 +0100

    Update UserAgents.cs

--- C#/UserAgents.cs
+++ C#/UserAgents.cs
@@ -7,7 +7,7 @@ namespace .Common
 		public const string HeaderName = "User-Agent";
 
 		public const string Firefox_92_Windows = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0";
-		public const string Edge_93_Windows = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36 Edg/93.0.961.38";
+		public const string Edge_96_Windows = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36 Edg/96.0.1054.29";
 		public const string Edge_97_Linux = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4688.0 Safari/537.36 Edg/97.0.1069.0";
 		public const string Safari_13_1_MacOSX = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15";
 		public const string Chrome_85_Windows = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36";
@@ -22,10 +22,11 @@ namespace .Common
 			return System.Environment.TickCount.ToString().Last() switch
 			{
 				'1' => Firefox_92_Windows,
-				'2' => Edge_93_Windows,
-				'3' => Safari_13_1_MacOSX,
-				'4' => Chrome_85_Windows,
-				'5' => Opera_66_Windows,
+				'2' => Edge_96_Windows,
+				'3' => Edge_97_Linux,
+				'4' => Safari_13_1_MacOSX,
+				'5' => Chrome_85_Windows,
+				'6' => Opera_66_Windows,
 				_ => Firefox_78ESR_Linux
 			};
 		}
