9f53fc2116acb9751b7bc22302b4dabcdbe95b72
Kingloo <k1ngloo@hotmail.com>
2022-01-23 18:06:11 +0100

    Update UserAgents

--- C#/UserAgents.cs
+++ C#/UserAgents.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace .Common
@@ -6,29 +8,32 @@ namespace .Common
 	{
 		public const string HeaderName = "User-Agent";
 
-		public const string Firefox_94_Windows = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0";
-		public const string Edge_96_Windows = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36 Edg/96.0.1054.29";
-		public const string Edge_97_Linux = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4688.0 Safari/537.36 Edg/97.0.1069.0";
-		public const string Safari_13_1_MacOSX = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15";
-		public const string Chrome_85_Windows = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36";
-		public const string Opera_66_Windows = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36 OPR/66.0.3515.72";
-		public const string Firefox_78ESR_Linux = "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0";
+		public const string Firefox_96_Windows = nameof(Firefox_96_Windows);
+		public const string Firefox_91_ESR_Linux = nameof(Firefox_91_ESR_Linux);
+		public const string Edge_97_Windows = nameof(Edge_97_Windows);
+		public const string Edge_99_Linux = nameof(Edge_99_Linux);
+		public const string Safari_13_1_MacOSX = nameof(Safari_13_1_MacOSX);
+		public const string Chrome_85_Windows = nameof(Chrome_85_Windows);
+		public const string Opera_66_Windows = nameof(Opera_66_Windows);
 
-		[System.Diagnostics.DebuggerStepThrough]
+		public static readonly IDictionary<string, string> Agents = new Dictionary<string, string>
+		{
+			{ Firefox_96_Windows, "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0" },
+			{ Firefox_91_ESR_Linux, "Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0" },
+			{ Edge_97_Windows, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36 Edg/97.0.1072.69" },
+			{ Edge_99_Linux, "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4814.0 Safari/537.36 Edg/99.0.1135.6" },
+			{ Safari_13_1_MacOSX, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15" },
+			{ Chrome_85_Windows, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36" },
+			{ Opera_66_Windows, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36 OPR/66.0.3515.72" }
+		};
+
 		public static string GetRandomUserAgent()
 		{
-			// .TickCount, measured in milliseconds, increments so quickly that the last digit is random enough for our needs
+			Random random = new Random();
 
-			return System.Environment.TickCount.ToString().Last() switch
-			{
-				'1' => Firefox_94_Windows,
-				'2' => Edge_96_Windows,
-				'3' => Edge_97_Linux,
-				'4' => Safari_13_1_MacOSX,
-				'5' => Chrome_85_Windows,
-				'6' => Opera_66_Windows,
-				_ => Firefox_78ESR_Linux
-			};
+			int randomNumber = random.Next(0, Agents.Count - 1);
+
+			return Agents.Values.ToArray()[randomNumber];
 		}
 	}
 }
