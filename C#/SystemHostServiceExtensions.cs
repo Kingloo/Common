fad7eeb773c97fe87b5de98318a976eff07c38c4
Kingloo <k1ngloo@hotmail.com>
2022-01-28 01:58:58 +0100

    Automatically add SystemD console logger when running as a SystemD service

--- C#/SystemHostServiceExtensions.cs
+++ C#/SystemHostServiceExtensions.cs
@@ -1,61 +1,79 @@
 using System;
 using System.Linq;
 using System.Runtime.Serialization;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Microsoft.Extensions.Hosting
 {
-    public static class SystemHostServiceExtensions
-    {
-        public class SystemHostServiceException : Exception
-        {
-            public SystemHostServiceException() { }
+	public static class SystemHostServiceExtensions
+	{
+		public class SystemHostServiceException : Exception
+		{
+			public SystemHostServiceException() { }
 
-            public SystemHostServiceException(string? message)
-                : base(message)
-            { }
+			public SystemHostServiceException(string? message)
+				: base(message)
+			{ }
 
-            public SystemHostServiceException(string? message, Exception? innerException)
-                : base(message, innerException)
-            { }
+			public SystemHostServiceException(string? message, Exception? innerException)
+				: base(message, innerException)
+			{ }
 
-            protected SystemHostServiceException(SerializationInfo info, StreamingContext context)
-                : base(info, context)
-            { }
-        }
+			protected SystemHostServiceException(SerializationInfo info, StreamingContext context)
+				: base(info, context)
+			{ }
+		}
 
-        public static IHostBuilder ConfigureSystemHostService(this IHostBuilder hostBuilder, string[] args)
-        {
-            if (IsSystemd(args) && IsWindowsService(args))
-            {
-                throw new SystemHostServiceException("cannot specify both SystemD and Windows Service");
-            }
-            
-            if (IsSystemd(args))
-            {
-                return hostBuilder.UseSystemd();
-            }
-            
-            if (IsWindowsService(args))
-            {
-                return hostBuilder.UseWindowsService();
-            }
-            
-            return hostBuilder;
-        }
+		public static IHostBuilder ConfigureSystemHostService(this IHostBuilder hostBuilder, string[] args)
+		{
+			return ConfigureSystemHostService(hostBuilder, args, addSystemdConsoleLogger: true);
+		}
 
-        private static bool IsSystemd(string[] args)
-        {
-            return ContainsArg(args, "--systemd");
-        }
+		public static IHostBuilder ConfigureSystemHostService(this IHostBuilder hostBuilder, string[] args, bool addSystemdConsoleLogger)
+		{
+			if (IsSystemd(args) && IsWindowsService(args))
+			{
+				throw new SystemHostServiceException("cannot specify both SystemD and Windows Service");
+			}
 
-        private static bool IsWindowsService(string[] args)
-        {
-            return ContainsArg(args, "--windowsservice");
-        }
+			if (IsSystemd(args))
+			{
+				if (addSystemdConsoleLogger)
+				{
+					hostBuilder.ConfigureServices((ctx, services) =>
+					{
+						services.AddLogging(logging =>
+						{
+							logging.AddSystemdConsole();
+						});
+					});
+				}
 
-        private static bool ContainsArg(string[] args, string arg)
-        {
-            return args.Any(a => a.ToLower().Equals(arg, StringComparison.Ordinal));
-        }
-    }
+				return hostBuilder.UseSystemd();
+			}
+
+			if (IsWindowsService(args))
+			{
+				return hostBuilder.UseWindowsService();
+			}
+
+			return hostBuilder;
+		}
+
+		private static bool IsSystemd(string[] args)
+		{
+			return ContainsArg(args, "--systemd");
+		}
+
+		private static bool IsWindowsService(string[] args)
+		{
+			return ContainsArg(args, "--windowsservice");
+		}
+
+		private static bool ContainsArg(string[] args, string arg)
+		{
+			return args.Any(a => a.ToLower().Equals(arg, StringComparison.Ordinal));
+		}
+	}
 }
