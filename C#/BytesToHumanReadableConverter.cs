d69a46e6a0b2dd39c2a178cca2d388f39966bdb9
Kingloo <k1ngloo@hotmail.com>
2022-11-06 16:44:55 +0100

    Update BytesToHumanReadableConverter

--- C#/BytesToHumanReadableConverter.cs
+++ C#/BytesToHumanReadableConverter.cs
@@ -1,51 +1,46 @@
-﻿using System;
-using System.Globalization;
-using System.Windows.Data;
+﻿using System.Globalization;
 
-namespace .Converters
+namespace .Common
 {
-    [ValueConversion(typeof(Int64), typeof(string))]
-    public class BytesToHumanReadableConverter : IValueConverter
-    {
-        private const decimal oneKiB = 1024m;
-        private const decimal oneMiB = oneKiB * oneKiB;
-        private const decimal oneGiB = oneKiB * oneMiB;
-        private const decimal oneTiB = oneKiB * oneGiB;
-        private const decimal onePiB = oneKiB * oneTiB;
-        private const decimal oneEiB = oneKiB * onePiB;
-        private const decimal oneZiB = oneKiB * oneEiB;
-        private const decimal oneYiB = oneKiB * oneZiB;
+	internal static class BytesToHumanReadableConverter
+	{
+		private const decimal oneKiB = 1024m;
+		private const decimal oneMiB = oneKiB * oneKiB;
+		private const decimal oneGiB = oneKiB * oneMiB;
+		private const decimal oneTiB = oneKiB * oneGiB;
+		private const decimal onePiB = oneKiB * oneTiB;
+		private const decimal oneEiB = oneKiB * onePiB;
+		private const decimal oneZiB = oneKiB * oneEiB;
+		private const decimal oneYiB = oneKiB * oneZiB;
 
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var cc = CultureInfo.CurrentCulture;
+		internal static string Format(long bytes)
+			=> Format(bytes, CultureInfo.CurrentCulture);
 
-            if (value is Int64 bytesLong)
-            {
-                decimal bytes = System.Convert.ToDecimal(bytesLong);
+		internal static string Format(long bytes, CultureInfo cultureInfo)
+		{
+			decimal decimalBytes = System.Convert.ToDecimal(bytes);
 
-                return bytes switch
-                {
-                    _ when bytes < oneKiB => string.Format(cc, "{0} bytes", bytes),
-                    _ when bytes < oneMiB => string.Format(cc, "{0:0.##} KiB", bytes / oneKiB),
-                    _ when bytes < oneGiB => string.Format(cc, "{0:0.##} MiB", bytes / oneMiB),
-                    _ when bytes < oneTiB => string.Format(cc, "{0:0.###} GiB", bytes / oneGiB),
-                    _ when bytes < onePiB => string.Format(cc, "{0:0.###} TiB", bytes / oneTiB),
-                    _ when bytes < oneEiB => string.Format(cc, "{0:0.###} PiB", bytes / onePiB),
-                    _ when bytes < oneZiB => string.Format(cc, "{0:0.###} EiB", bytes / oneEiB),
-                    _ when bytes < oneYiB => string.Format(cc, "{0:0.###} ZiB", bytes / oneZiB),
-                    _ => string.Format(cc, "{0} YiB", bytes / oneYiB)
-                };
-            }
-            else
-            {
-                throw new ArgumentException($"method requires {typeof(Int64)}, you provided {value.GetType()}", nameof(value));
-            }
-        }
+			return bytes switch
+			{
+				_ when bytes < oneKiB => FormatNumber(decimalBytes, "bytes", 0, cultureInfo),
+				_ when bytes < oneMiB => FormatNumber(decimalBytes / oneKiB, "KiB", 2, cultureInfo),
+				_ when bytes < oneGiB => FormatNumber(decimalBytes / oneMiB, "MiB", 2, cultureInfo),
+				_ when bytes < oneTiB => FormatNumber(decimalBytes / oneGiB, "GiB", 2, cultureInfo),
+				_ when bytes < onePiB => FormatNumber(decimalBytes / oneTiB, "TiB", 2, cultureInfo),
+				_ when bytes < oneEiB => FormatNumber(decimalBytes / onePiB, "PiB", 3, cultureInfo),
+				_ when bytes < oneZiB => FormatNumber(decimalBytes / oneEiB, "EiB", 3, cultureInfo),
+				_ when bytes < oneYiB => FormatNumber(decimalBytes / oneZiB, "ZiB", 3, cultureInfo),
+				_ => FormatNumber(decimalBytes / oneYiB, "YiB", 3, cultureInfo)
+			};
+		}
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotSupportedException();
-        }
-    }
+		private static string FormatNumber(decimal number, string unit, uint decimalPlaces, CultureInfo cultureInfo)
+		{
+			return string.Format(
+				cultureInfo,
+				"{0} {1}",
+				number.ToString($"N{decimalPlaces}", cultureInfo),
+				unit);
+		}
+	}
 }
