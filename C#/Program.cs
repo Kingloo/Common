a7c57dadb5d82cba254d37ba6d72e465e36eef1b
Kingloo <k1ngloo@hotmail.com>
2023-04-07 00:42:03 +0200

    Add Program.cs template

---
+++ C#/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+#pragma warning disable IDE0090
+
+namespace 
+{
+    public static class Program
+    {
+        private static readonly CancellationTokenSource cts = new CancellationTokenSource();
+
+        private static void CancelHandler(object? sender, ConsoleCancelEventArgs e)
+        {
+            e.Cancel = false;
+
+            cts.Cancel();
+        }
+
+        public static async Task<int> Main(string[] args)
+        {
+            Console.CancelKeyPress += CancelHandler;
+
+            int exitCode = 0;
+
+            try
+            {
+                exitCode = await InnerProgram.InnerMain(args, cts.Token).ConfigureAwait(false);
+            }
+            catch (OperationCanceledException)
+            {
+                await Console.Out.WriteLineAsync("cancelled").ConfigureAwait(false);
+
+                exitCode = -1;
+            }
+            finally
+            {
+                cts.Dispose();
+
+                Console.CancelKeyPress -= CancelHandler;
+            }
+
+            return exitCode;
+        }
+    }
+}
+
+#pragma warning restore IDE0090
