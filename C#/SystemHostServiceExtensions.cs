d10d8502007fb435405a5a6a6600c2029d6371ad
Kingloo <k1ngloo@hotmail.com>
2023-07-13 02:33:52 +0200

    Simplify significantly

--- C#/SystemHostServiceExtensions.cs
+++ C#/SystemHostServiceExtensions.cs
@@ -1,85 +1,27 @@
 using System;
-using System.Linq;
-using System.Runtime.Serialization;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting.Systemd;
+using Microsoft.Extensions.Hosting.WindowsServices;
 
-namespace 
+namespace .Common
 {
-	public class SystemHostServiceException : Exception
-	{
-		public SystemHostServiceException() { }
-
-		public SystemHostServiceException(string? message)
-			: base(message)
-		{ }
-
-		public SystemHostServiceException(string? message, Exception? innerException)
-			: base(message, innerException)
-		{ }
-
-		protected SystemHostServiceException(SerializationInfo info, StreamingContext context)
-			: base(info, context)
-		{ }
-	}
-
 	public static class SystemHostServiceExtensions
 	{
-		public static IHostBuilder ConfigureSystemHostService(this IHostBuilder hostBuilder, string[] args)
+		public static IHostBuilder ConfigureSystemHostService(this IHostBuilder hostBuilder)
 		{
-			return ConfigureSystemHostService(hostBuilder, args, addSystemdConsoleLogger: true);
-		}
+			ArgumentNullException.ThrowIfNull(hostBuilder);
 
-		public static IHostBuilder ConfigureSystemHostService(this IHostBuilder hostBuilder, string[] args, bool addSystemdConsoleLogger)
-		{
-			if (hostBuilder is null)
+			if (SystemdHelpers.IsSystemdService())
 			{
-				throw new ArgumentNullException(nameof(hostBuilder));
-			}
-
-			if (IsSystemd(args) && IsWindowsService(args))
-			{
-				throw new SystemHostServiceException("cannot specify both SystemD and Windows Service");
-			}
-
-			if (IsSystemd(args))
-			{
-				if (addSystemdConsoleLogger)
-				{
-					hostBuilder.ConfigureServices((ctx, services) =>
-					{
-						services.AddLogging(logging =>
-						{
-							logging.AddSystemdConsole();
-						});
-					});
-				}
-
 				return hostBuilder.UseSystemd();
 			}
 
-			if (IsWindowsService(args))
+			if (WindowsServiceHelpers.IsWindowsService())
 			{
 				return hostBuilder.UseWindowsService();
 			}
 
 			return hostBuilder;
-		}
-
-		private static bool IsSystemd(string[] args)
-		{
-			return ContainsArg(args, "--systemd");
-		}
-
-		private static bool IsWindowsService(string[] args)
-		{
-			return ContainsArg(args, "--windowsservice");
-		}
-
-		private static bool ContainsArg(string[] args, string arg)
-		{
-			return args.Any(a => a.Equals(arg, StringComparison.OrdinalIgnoreCase));
 		}
 	}
 }
