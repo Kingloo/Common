c7821bb3ebfb5559aeead10cef93ac464e9bc15a
Kingloo <k1ngloo@hotmail.com>
2023-11-22 15:13:59 +0100

    LinqExtensions
    IEnumerable.Randomise is better.

--- DotNet/C#/Extensions/LinqExtensions.cs
+++ DotNet/C#/Extensions/LinqExtensions.cs
@@ -1,23 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using static System.Security.Cryptography.RandomNumberGenerator;
 
 namespace .Extensions
 {
 	public static class LinqExtensions
 	{
-		public static IList<T> Randomise<T>(this IList<T> list)
-		{
-			Random random = new Random();
-
-			return list.OrderBy(x => random.Next()).ToList();
-		}
-
 		public static IEnumerable<T> Randomise<T>(this IEnumerable<T> enumerable)
 		{
-			Random random = new Random();
-
-			return enumerable.ToList().OrderBy(x => random.Next());
+			return enumerable.OrderBy(static x => GetInt32(Int32.MaxValue));
 		}
 	}
 }
