f0a890862dd64720488594680c6bb56a5577a7d6
Kingloo <k1ngloo@hotmail.com>
2024-01-01 15:07:28 +0100

    Change RID from win10-x64 to win-x64
    RIDs were simplified in NET 8.

--- DotNet/publish-desktop-x64.ps1
+++ DotNet/publish-desktop-x64.ps1
@@ -1,3 +1,3 @@
 dotnet restore
 dotnet build .\{SOLUTION}.sln -c Release --no-restore --nologo
-dotnet publish .\path\to\{PROJECT}.csproj -c Release -r win10-x64 /p:PublishSingleFile=true --no-self-contained --no-restore
+dotnet publish .\path\to\{PROJECT}.csproj -c Release -r win-x64 /p:PublishSingleFile=true --no-self-contained --no-restore
